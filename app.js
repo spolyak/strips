e17e0d4875c6920cf69370526d3a89b031a6de8a
Steve Polyak <stevepolyak@mac.com>
2017-11-02 15:45:51 -0500

    working on science ideas

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var strips = require('strips');
 
 // Load the domain and problem.
-strips.load('./examples/blocksworld2/domain.txt', './examples/blocksworld2/problem.txt', function(domain, problem) {
+strips.load('./examples/science/s1-domain.txt', './examples/science/s1-p1-problem.txt', function(domain, problem) {
     // Run the problem against the domain.
-    var solutions = strips.solve(domain, problem);
+    var solutions = strips.solve(domain, problem, false, 1000);
 
     // Display each solution.
     for (var i in solutions) {
